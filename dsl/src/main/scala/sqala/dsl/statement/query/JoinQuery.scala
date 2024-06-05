f3a605b4576137b802b25ed0bf6f755e8264f7f0
wz7982 <zhanggev5@hotmail.com>
2024-06-05 09:05:51 +0800

    support using clause

--- dsl/src/main/scala/sqala/dsl/statement/query/JoinQuery.scala
+++ dsl/src/main/scala/sqala/dsl/statement/query/JoinQuery.scala
@@ -1,8 +1,9 @@
 package sqala.dsl.statement.query
 
+import sqala.ast.expr.SqlExpr
 import sqala.ast.statement.SqlQuery
 import sqala.ast.table.{SqlJoinCondition, SqlTable}
-import sqala.dsl.Expr
+import sqala.dsl.{Column, Expr}
 
 class JoinQuery[T](
     private[sqala] val tables: T,
@@ -12,4 +13,9 @@ class JoinQuery[T](
     def on(f: T => Expr[Boolean]): SelectQuery[T] =
         val sqlCondition = f(tables).asSqlExpr
         val sqlTable = table.map(_.copy(condition = Some(SqlJoinCondition.On(sqlCondition))))
+        SelectQuery(tables, ast.copy(from = sqlTable.toList))
+
+    def `using`[E](f: T => Column[E]): SelectQuery[T] =
+        val sqlCondition = f(tables).asSqlExpr.asInstanceOf[SqlExpr.Column].copy(tableName = None)
+        val sqlTable = table.map(_.copy(condition = Some(SqlJoinCondition.Using(sqlCondition))))
         SelectQuery(tables, ast.copy(from = sqlTable.toList))
