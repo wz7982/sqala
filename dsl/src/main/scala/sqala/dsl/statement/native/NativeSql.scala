94a9ad9b7ddfdff115a46dadc9f5ce17c57514be
wz7982 <zhanggev5@hotmail.com>
2024-06-22 23:07:27 +0800

    optimize NativeSql

--- dsl/src/main/scala/sqala/dsl/statement/native/NativeSql.scala
+++ dsl/src/main/scala/sqala/dsl/statement/native/NativeSql.scala
@@ -1,3 +1,8 @@
 package sqala.dsl.statement.native
 
-case class NativeSql(sql: String, args: Array[Any])
+case class NativeSql(sql: String, args: Array[Any]):
+    def +(that: NativeSql): NativeSql =
+        NativeSql(sql + that.sql, args.appendedAll(that.args))
+
+    def +(s: String): NativeSql =
+        NativeSql(sql + s, args)
