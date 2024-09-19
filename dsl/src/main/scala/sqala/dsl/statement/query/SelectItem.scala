3cdfa70ad951989dc88364fcc935a53e05fb51e0
wz7982 <zhanggev5@hotmail.com>
2024-09-19 11:16:13 +0800

    projection does not allow nested tuples

--- dsl/src/main/scala/sqala/dsl/statement/query/SelectItem.scala
+++ dsl/src/main/scala/sqala/dsl/statement/query/SelectItem.scala
@@ -30,28 +30,28 @@ object SelectItem:
         def selectItems(item: Expr[T, K], cursor: Int): List[SqlSelectItem.Item] = 
             SqlSelectItem.Item(item.asSqlExpr, Some(s"c${cursor}")) :: Nil
 
-    transparent inline given tupleSelectItem[H, T <: Tuple](using sh: SelectItem[H], st: SelectItem[T]): SelectItem[H *: T] = new SelectItem[H *: T]:
+    transparent inline given tupleSelectItem[X, K <: ExprKind, T <: Tuple](using sh: SelectItem[Expr[X, K]], st: SelectItem[T]): SelectItem[Expr[X, K] *: T] = new SelectItem[Expr[X, K] *: T]:
         type R = sh.R *: ToTuple[st.R]
 
-        def subQueryItems(item: H *: T, cursor: Int, alias: String): R =
+        def subQueryItems(item: Expr[X, K] *: T, cursor: Int, alias: String): R =
             val head = sh.subQueryItems(item.head, cursor, alias)
             val tail = st.subQueryItems(item.tail, cursor + sh.offset(item.head), alias) match
                 case x: Tuple => x
             (head *: tail).asInstanceOf[R]
 
-        def offset(item: H *: T): Int = sh.offset(item.head) + st.offset(item.tail)
+        def offset(item: Expr[X, K] *: T): Int = sh.offset(item.head) + st.offset(item.tail)
 
-        def selectItems(item: H *: T, cursor: Int): List[SqlSelectItem.Item] =
+        def selectItems(item: Expr[X, K] *: T, cursor: Int): List[SqlSelectItem.Item] =
             sh.selectItems(item.head, cursor) ++ st.selectItems(item.tail, cursor + sh.offset(item.head))
 
-    transparent inline given tuple1SelectItem[H](using sh: SelectItem[H]): SelectItem[H *: EmptyTuple] = new SelectItem[H *: EmptyTuple]:
+    transparent inline given tuple1SelectItem[X, K <: ExprKind](using sh: SelectItem[Expr[X, K]]): SelectItem[Expr[X, K] *: EmptyTuple] = new SelectItem[Expr[X, K] *: EmptyTuple]:
         type R = sh.R *: EmptyTuple
 
-        def subQueryItems(item: H *: EmptyTuple, cursor: Int, alias: String): R =
+        def subQueryItems(item: Expr[X, K] *: EmptyTuple, cursor: Int, alias: String): R =
             val head = sh.subQueryItems(item.head, cursor, alias)
             head *: EmptyTuple
 
-        def offset(item: H *: EmptyTuple): Int = sh.offset(item.head)
+        def offset(item: Expr[X, K] *: EmptyTuple): Int = sh.offset(item.head)
 
-        def selectItems(item: H *: EmptyTuple, cursor: Int): List[SqlSelectItem.Item] =
+        def selectItems(item: Expr[X, K] *: EmptyTuple, cursor: Int): List[SqlSelectItem.Item] =
             sh.selectItems(item.head, cursor)
