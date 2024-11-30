988452ce7a368ab7d4a85079d1f310eba15a8202
wz7982 <zhanggev5@hotmail.com>
2024-11-30 17:19:18 +0800

    enhance primary key security

--- dsl/src/main/scala/sqala/dsl/annotation/Annotation.scala
+++ dsl/src/main/scala/sqala/dsl/annotation/Annotation.scala
@@ -1,6 +1,7 @@
 package sqala.dsl.annotation
 
-import scala.annotation.StaticAnnotation
+import scala.annotation.{StaticAnnotation, MacroAnnotation}
+import scala.quoted.Quotes
 
 @scala.annotation.meta.field
 case class table(tableName: String) extends StaticAnnotation
@@ -9,10 +10,54 @@ case class table(tableName: String) extends StaticAnnotation
 case class column(columnName: String) extends StaticAnnotation
 
 @scala.annotation.meta.field
-class primaryKey extends StaticAnnotation
+class primaryKey extends MacroAnnotation:
+    def transform(using 
+        q: Quotes
+    )(
+        definition: q.reflect.Definition, 
+        companion: Option[q.reflect.Definition]
+    ): List[q.reflect.Definition] =
+        import q.reflect.*
+
+        val t = definition.symbol.typeRef.asType
+
+        t match
+            case '[Option[t]] =>
+                report.error:
+                    "Primary key must be not empty."
+            case _ =>
+
+        definition :: Nil
 
 @scala.annotation.meta.field
-class autoInc extends StaticAnnotation
+class autoInc extends MacroAnnotation:
+    def transform(using 
+        q: Quotes
+    )(
+        definition: q.reflect.Definition, 
+        companion: Option[q.reflect.Definition]
+    ): List[q.reflect.Definition] =
+        import q.reflect.*
+
+        val t = definition.symbol.typeRef.asType
+
+        t match
+            case '[Int] =>
+            case '[Long] =>
+            case _ => 
+                report.error:
+                    "Auto-increment primary key must be of type Int or Long."
+
+        val owner = definition.symbol.owner
+        val fields = owner.declaredFields
+        
+        for f <- fields if f.name != definition.name do
+            f.annotations.foreach:
+                case Apply(Select(New(TypeIdent("autoInc")), _), _) =>
+                    report.error(s"There can only one auto-increment primary key.")
+                case _ =>
+
+        definition :: Nil
 
 @scala.annotation.meta.field
 class sqlFunction extends StaticAnnotation
