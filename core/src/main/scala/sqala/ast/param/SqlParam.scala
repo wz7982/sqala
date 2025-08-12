4533d1d17fb6e0909bd42e37a997a3e77234a9fb
wz7982 <zhanggev5@hotmail.com>
2025-08-12 17:54:59 +0800

    optimize code

--- core/src/main/scala/sqala/ast/param/SqlParam.scala
+++ core/src/main/scala/sqala/ast/param/SqlParam.scala
@@ -3,4 +3,4 @@ package sqala.ast.param
 enum SqlParam(val param: String):
     case All extends SqlParam("ALL")
     case Distinct extends SqlParam("DISTINCT")
-    case Custom(p: String) extends SqlParam(p)
+    case Custom(override val param: String) extends SqlParam(param)
