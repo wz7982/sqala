5e6b250a529e9fd413e35bb1618d86a2772e31bf
wz7982 <zhanggev5@hotmail.com>
2025-09-11 10:35:55 +0800

    new dsl

--- static/src/main/scala/sqala/static/metadata/CustomField.scala
+++ static/src/main/scala/sqala/static/metadata/CustomField.scala
@@ -1,6 +1,7 @@
 package sqala.static.metadata
 
 import sqala.ast.expr.SqlExpr
+import sqala.ast.expr.SqlType
 
 trait CustomField[T, R](using a: AsSqlExpr[R]) extends AsSqlExpr[T]:
     def toValue(x: T): R
@@ -8,3 +9,5 @@ trait CustomField[T, R](using a: AsSqlExpr[R]) extends AsSqlExpr[T]:
     def fromValue(x: R): T
         
     def asSqlExpr(x: T): SqlExpr = a.asSqlExpr(toValue(x))
+
+    def sqlType: SqlType = a.sqlType
